50d524ab37629ccc20c02ff4fb418b49ea8272f2
youFireFly <youFireFly>
2021-04-02 20:24:00 +0800

    测试提交项目文件

--- baozidemo/src/main/java/com/example/baozidemo/BaozidemoApplication.java
+++ baozidemo/src/main/java/com/example/baozidemo/BaozidemoApplication.java
@@ -8,7 +8,7 @@ public class BaozidemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BaozidemoApplication.class, args);
-        System.out.println("lala");
+        System.out.println("lala2");
     }
 
 }
