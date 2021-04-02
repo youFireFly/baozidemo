27d6244345aa6b7bc2b44f8c5926e9a64ebbd1d5
DESKTOP-NQ62PEU\Ruiz-36 <1425476994@qq.com>
2021-04-02 10:50:27 +0800

    测试项目

---
+++ baozidemo/src/main/java/com/example/baozidemo/BaozidemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.baozidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaozidemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaozidemoApplication.class, args);
+    }
+
+}
---
+++ baozidemo/src/test/java/com/example/baozidemo/BaozidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baozidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaozidemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
